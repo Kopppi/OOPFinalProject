80bf6b20b8fef60805903e87ae0803e014203045
veetiengblom <Veeti_engblom@hotmail.com>
2022-04-28 13:32:23 +0300

    Added Company fragment and list_item_layout

---
+++ app/src/main/java/com/example/navdraw/ui/company/CompanyFragment.java
@@ -0,0 +1,25 @@
+package com.example.navdraw.ui.company;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import com.example.navdraw.R;
+
+import com.example.navdraw.TaxSample;
+import com.example.navdraw.databinding.FragmentFavouritesBinding;
+
+public class CompanyFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_company, container, false);
+
+        return rootView;
+    }
+}
