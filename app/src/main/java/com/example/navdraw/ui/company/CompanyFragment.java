d561fffa4877be61723561ef3bf9e99b5e3398b8
veetiengblom <Veeti_engblom@hotmail.com>
2022-04-29 15:44:34 +0300

    Modified: CompanyFragment writing to file app solmuun

--- app/src/main/java/com/example/navdraw/ui/company/CompanyFragment.java
+++ app/src/main/java/com/example/navdraw/ui/company/CompanyFragment.java
@@ -1,5 +1,6 @@
 package com.example.navdraw.ui.company;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +19,10 @@ import com.example.navdraw.TaxSample;
 import com.example.navdraw.ui.favourites.FavouritesList;
 import com.example.navdraw.ui.home.HomeFragment;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CompanyFragment extends Fragment {
@@ -25,6 +30,7 @@ public class CompanyFragment extends Fragment {
     String a;
     int b = 0;
     int position;
+    String CSVData = "";
     TextView textViewName;
     TextView textViewTaxed;
     TextView textViewYID;
@@ -34,11 +40,13 @@ public class CompanyFragment extends Fragment {
     Button buttonAddFavourites;
     private List<TaxSample> TaxSamples;
     private List<FavouritesList> Favourites;
+    boolean helper;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_company, container, false);
         TaxSamples = TaxList.getInstance().getTaxSamples();
+        Favourites = new ArrayList<>();
 
         textViewName = rootView.findViewById(R.id.textViewName);
         textViewTaxed = rootView.findViewById(R.id.textViewTaxed);
@@ -77,15 +85,46 @@ public class CompanyFragment extends Fragment {
         buttonAddFavourites.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FavouritesList company = new FavouritesList();
-                company.setID(TaxSamples.get(position).getID());
-                company.setName(TaxSamples.get(position).getName());
-                company.setLocation(TaxSamples.get(position).getLocation());
-                company.setTaxedIncome(TaxSamples.get(position).getTaxedIncome());
-                company.setPayedTax(TaxSamples.get(position).getPayedTax());
-                Toast toast=Toast. makeText(getContext(), TaxSamples.get(position).getName() + " Added to favourites",Toast. LENGTH_SHORT);
-                toast. setMargin(50,50);
-                toast. show();
+                System.out.println(TaxSamples.get(position).getID());
+                System.out.println(Favourites.size());
+                for (int i = 0; i < 5; i++) {
+                    System.out.println(Favourites.get(i).getID());
+                }
+                for (int i = 0; i < Favourites.size(); i++) {
+                    if (TaxSamples.get(position).getID() == Favourites.get(i).getID()){
+                        helper = false;
+                    } else {
+                        helper = true;
+                    }
+                }
+                if (helper) {
+                    FavouritesList company = new FavouritesList();
+                    company.setID(TaxSamples.get(position).getID());
+                    company.setName(TaxSamples.get(position).getName());
+                    company.setLocation(TaxSamples.get(position).getLocation());
+                    company.setTaxedIncome(TaxSamples.get(position).getTaxedIncome());
+                    company.setPayedTax(TaxSamples.get(position).getPayedTax());
+                    Favourites.add(company);
+                    Toast toast=Toast. makeText(getContext(), TaxSamples.get(position).getName() + " Added to favourites",Toast. LENGTH_SHORT);
+                    toast. setMargin(50,50);
+                    toast. show();
+
+                    for(int i = 0; i < Favourites.size(); i++){
+                        CSVData += Favourites.get(i).getID() + ";" + TaxSamples.get(position).getLocation() + ";" + Favourites.get(i).getName()
+                                + ";" + Favourites.get(i).getPayedTax() + ";" + Favourites.get(i).getTaxedIncome() + "\n";
+                    }
+                    System.out.println(CSVData);
+
+                    try {
+                        OutputStreamWriter osw = new OutputStreamWriter(getActivity().getApplicationContext().openFileOutput("favourites.csv", Context.MODE_APPEND));
+                        osw.append(CSVData);
+                        osw.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
         return rootView;
